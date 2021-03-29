21b0543bd1533fe4c4425d9b02924e7dc940f435
RedKissiFrott <redkissifrott@yahoo.fr>
2021-03-29 15:00:06 +0200

    Initial commit

---
+++ src/main/java/fr/redkissifrott/safetynetAlerts/SafetynetAlertApplication.java
@@ -0,0 +1,13 @@
+package fr.redkissifrott.safetynetAlerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/redkissifrott/safetynetAlerts/SafetynetAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.redkissifrott.safetynetAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
